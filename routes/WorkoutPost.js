a46d417cefd1387a6f06cd5316d29144367b9ffb
Romeo Barbieto Junior <163235833+romeojuniorb@users.noreply.github.com>
2024-12-08 23:03:05 -0500

    Update WorkoutPost.js

--- routes/WorkoutPost.js
+++ routes/WorkoutPost.js
@@ -1,22 +1,64 @@
-import mongoose from "mongoose";
-import BasePost from "./BasePost.js";
+import express from "express";
+import WorkoutPost from "../models/WorkoutPost.js";
+import ExpressError from "../utils/ExpressError.js";
+import { isLoggedIn } from "../utils/middleware.js"; 
 
-const { Schema } = mongoose;
+const router = express.Router();
 
-const workoutSchema = new Schema({
-  exercises: [
-    {
-      name: { type: String, required: true },
-      sets: { type: Number },
-      reps: { type: Number },
-      duration: { type: Number }, // In minutes
-      weight: { type: Number } // In kilograms
+router.get("/", async (req, res, next) => {
+    try {
+        const workoutPosts = await WorkoutPost.find({ type: "workout" });
+        res.render("posts/workoutPosts/index", { workoutPosts });
+    } catch (err) {
+        next(new ExpressError("Error fetching workout posts.", 500));
     }
-  ],
-  totalDuration: { type: Number, required: true }, // Total duration in minutes
-  caloriesBurned: { type: Number, required: true }
 });
 
-const WorkoutPost = BasePost.discriminator("workout", workoutSchema);
+router.get("/new", isLoggedIn, (req, res) => {
+    res.render("posts/workoutPosts/new");
+});
 
-export default WorkoutPost;
+router.post("/", isLoggedIn, async (req, res, next) => {
+    try {
+        const newWorkoutPost = new WorkoutPost({ ...req.body, type: "workout" });
+        await newWorkoutPost.save();
+        res.redirect("/workoutPosts");
+    } catch (err) {
+        next(new ExpressError("Error creating a new workout post.", 500));
+    }
+});
+
+router.get("/:id", async (req, res, next) => {
+    try {
+        const workoutPost = await WorkoutPost.findById(req.params.id);
+        if (!workoutPost) {
+            throw new ExpressError("Workout post not found.", 404);
+        }
+        res.render("posts/workoutPosts/show", { workoutPost });
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.get("/:id/edit", isLoggedIn, async (req, res, next) => {
+    try {
+        const workoutPost = await WorkoutPost.findById(req.params.id);
+        if (!workoutPost) {
+            throw new ExpressError("Workout post not found.", 404);
+        }
+        res.render("posts/workoutPosts/edit", { workoutPost });
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.delete("/:id", isLoggedIn, async (req, res, next) => {
+    try {
+        await WorkoutPost.findByIdAndDelete(req.params.id);
+        res.redirect("/workoutPosts");
+    } catch (err) {
+        next(new ExpressError("Error deleting workout post.", 500));
+    }
+});
+
+export default router;
